b1443625230fb55cec01dc9772d37d97cbb42d1b
lyzq7 <17623837281@163.com>
2025-09-13 13:38:35 +0800

    memory

--- kernel/vm.c
+++ kernel/vm.c
@@ -21,32 +21,36 @@ extern char trampoline[]; // trampoline.S
 void
 kvminit()
 {
-  kernel_pagetable = (pagetable_t) kalloc();
-  memset(kernel_pagetable, 0, PGSIZE);
-
-  // uart registers
-  kvmmap(UART0, UART0, PGSIZE, PTE_R | PTE_W);
-
-  // virtio mmio disk interface
-  kvmmap(VIRTIO0, VIRTIO0, PGSIZE, PTE_R | PTE_W);
-
-  // CLINT
-  kvmmap(CLINT, CLINT, 0x10000, PTE_R | PTE_W);
-
-  // PLIC
-  kvmmap(PLIC, PLIC, 0x400000, PTE_R | PTE_W);
-
-  // map kernel text executable and read-only.
-  kvmmap(KERNBASE, KERNBASE, (uint64)etext-KERNBASE, PTE_R | PTE_X);
-
-  // map kernel data and the physical RAM we'll make use of.
-  kvmmap((uint64)etext, (uint64)etext, PHYSTOP-(uint64)etext, PTE_R | PTE_W);
-
-  // map the trampoline for trap entry/exit to
-  // the highest virtual address in the kernel.
-  kvmmap(TRAMPOLINE, (uint64)trampoline, PGSIZE, PTE_R | PTE_X);
+  kernel_pagetable = kama_kvminit_newpgtbl();
 }
 
+pagetable_t
+kama_kvminit_newpgtbl()
+{    
+    pagetable_t pgtbl = (pagetable_t) kalloc();    
+    memset(pgtbl, 0, PGSIZE);
+    kama_kvm_map_pagetable(pgtbl);
+    return pgtbl;
+}
+
+void kama_kvm_map_pagetable(pagetable_t pgtbl) {    
+    // 将各种内核需要的 direct mapping 添加到页表 pgtbl 中        
+    // uart registers    
+    kvmmap(pgtbl, UART0, UART0, PGSIZE, PTE_R | PTE_W);
+        // virtio mmio disk interface    
+    kvmmap(pgtbl, VIRTIO0, VIRTIO0, PGSIZE, PTE_R | PTE_W);
+        // CLINT    
+    kvmmap(pgtbl, CLINT, CLINT, 0x10000, PTE_R | PTE_W);
+        // PLIC    
+    kvmmap(pgtbl, PLIC, PLIC, 0x400000, PTE_R | PTE_W);
+        // map kernel text executable and read-only.    
+    kvmmap(pgtbl, KERNBASE, KERNBASE, (uint64)etext-KERNBASE, PTE_R | PTE_X);
+        // map kernel data and the physical RAM we'll make use of.    
+    kvmmap(pgtbl, (uint64)etext, (uint64)etext, PHYSTOP-(uint64)etext, PTE_R | PTE_W);
+        // map the trampoline for trap entry/exit to    
+    // the highest virtual address in the kernel.    
+    kvmmap(pgtbl, TRAMPOLINE, (uint64)trampoline, PGSIZE, PTE_R | PTE_X);
+}
 // Switch h/w page table register to the kernel's page table,
 // and enable paging.
 void
@@ -56,6 +60,20 @@ kvminithart()
   sfence_vma();
 }
 
+// kernel/vm.c
+// 递归释放一个内核页表中的所有映射，但是不释放其指向的物理页
+void
+kama_kvm_free_kernelpgtbl(pagetable_t pagetable) {    
+for (int i = 0;i < 512;++i) {        
+pte_t pte = pagetable[i];        
+uint64 child = PTE2PA(pte);        
+if ((pte & PTE_V) && (pte & (PTE_R | PTE_W | PTE_X)) == 0) {      // 如果该页表项指向更低一级的页表            
+kama_kvm_free_kernelpgtbl((pagetable_t)child);                     // 递归释放低一级页表及其页表项            
+pagetable[i] = 0;
+        }
+    }    
+kfree((void*)pagetable);        // 释放当前级别页表所占用空间
+}
 // Return the address of the PTE in page table pagetable
 // that corresponds to virtual address va.  If alloc!=0,
 // create any required page-table pages.
@@ -115,10 +133,10 @@ walkaddr(pagetable_t pagetable, uint64 va)
 // only used when booting.
 // does not flush TLB or enable paging.
 void
-kvmmap(uint64 va, uint64 pa, uint64 sz, int perm)
-{
-  if(mappages(kernel_pagetable, va, sz, pa, perm) != 0)
-    panic("kvmmap");
+kvmmap(pagetable_t pgtbl, uint64 va, uint64 pa, uint64 sz, int perm)            
+{    
+  if(mappages(pgtbl, va, sz, pa, perm) != 0)        
+  panic("kvmmap");
 }
 
 // translate a kernel virtual address to
@@ -126,21 +144,19 @@ kvmmap(uint64 va, uint64 pa, uint64 sz, int perm)
 // addresses on the stack.
 // assumes va is page aligned.
 uint64
-kvmpa(uint64 va)
-{
-  uint64 off = va % PGSIZE;
-  pte_t *pte;
+kvmpa(pagetable_t pgtbl, uint64 va)         
+{    
+  uint64 off = va % PGSIZE;    
+  pte_t *pte;    
   uint64 pa;
-  
-  pte = walk(kernel_pagetable, va, 0);
-  if(pte == 0)
-    panic("kvmpa");
-  if((*pte & PTE_V) == 0)
-    panic("kvmpa");
-  pa = PTE2PA(*pte);
-  return pa+off;
+      pte = walk(pgtbl, va, 0);			//kernel_pagetable改为参数pgtbl    
+  if (pte == 0)        
+  panic("kvmpa");    
+  if ((*pte & PTE_V) == 0)        
+  panic("kvmpa");    
+  pa = PTE2PA(*pte);    
+  return pa + off;
 }
-
 // Create PTEs for virtual addresses starting at va that refer to
 // physical addresses starting at pa. va and size might not
 // be page-aligned. Returns 0 on success, -1 if walk() couldn't
